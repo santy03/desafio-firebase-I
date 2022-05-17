b49b13c6f453da69c9a461fc0845646eb562d582
santy03 <95034966+santy03@users.noreply.github.com>
2022-05-17 16:33:29 -0300

    Add files via upload

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -3,18 +3,18 @@ import { useParams } from "react-router";
 import customFetch from "../utils/CustomFetch";
 import ItemDetail from "./ItemDetail";
 import products from '../utils/products';
+import { firestoreFetchOne } from "../utils/fetchFirestore";
+
 
 const ItemDetailContainer = () => {
     const [dato, setDato] = useState({});
-    const {idItem} = useParams();
-    console.log(idItem);
+    const { idCategory } = useParams();
 
     useEffect(() => {
-        customFetch(2000, products.find(item =>item.id === parseInt(idItem)))
+        firestoreFetchOne(idCategory)
             .then(result => setDato(result))
             .catch(err => console.log(err))
     }, []);
-    console.log(dato);
     
     return (
         <ItemDetail item={dato} />
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -5,8 +5,8 @@ const ItemList = ({ items }) => {
     return (
         <ProductsContainer>
         {
-            items.length > 0?
-			items.map(item => <Item id={item.id} key={item.id} title={item.name} price={item.cost} pictureUrl={item.image[0]} stock={item.stock}/>)
+            items.length > 0
+			? items.map(item => <Item key={item.id} id={item.id} title={item.name} price={item.cost} pictureUrl={item.image[0]} stock={item.stock}/>)
 			: <p>Recalculando...</p>
         }
         </ProductsContainer>
